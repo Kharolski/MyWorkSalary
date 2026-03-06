b49b6b7155a034c4c6a4c9c3bb77597aabf2854e
Aleh Kharolski <Kharolski@gmail.com>
2026-03-06 02:48:14 +0100

    fix för ios ads visning

--- MyWorkSalary/Platforms/iOS/AppDelegate.cs
+++ MyWorkSalary/Platforms/iOS/AppDelegate.cs
@@ -1,10 +1,19 @@
 ﻿using Foundation;
+using Google.MobileAds;
 
 namespace MyWorkSalary
 {
     [Register("AppDelegate")]
     public class AppDelegate : MauiUIApplicationDelegate
     {
+        public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+        {
+            // Initialize AdMob for iOS
+            MobileAds.SharedInstance.Start(completionHandler: null);
+            
+            return base.FinishedLaunching(application, launchOptions);
+        }
+
         protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
     }
 }
